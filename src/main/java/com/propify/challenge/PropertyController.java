c894ec41eaf9ebd9d5eb6cb15e87207db6a657d3
brendsanchez <brendasanchez9310@gmail.com>
2022-09-05 00:17:30 -0300

    feat/querys mapper property

--- src/main/java/com/propify/challenge/PropertyController.java
+++ src/main/java/com/propify/challenge/PropertyController.java
@@ -1,14 +1,17 @@
 package com.propify.challenge;
 
 import com.propify.challenge.exception.NotValidException;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.Collection;
 
 @RestController
@@ -22,7 +25,7 @@ public class PropertyController {
 
     // API endpoints for CRUD operations on entities of type Property
     @GetMapping("/properties")
-    public ResponseEntity<Collection<Property>> search(String minRentPrice, String maxRentPrice) {
+    public ResponseEntity<Collection<Property>> search(@Param("minRentPrice") String minRentPrice, @Param("maxRentPrice") String maxRentPrice) {
         return ResponseEntity.ok().body(propertyService.search(minRentPrice, maxRentPrice));
     }
 
@@ -32,13 +35,13 @@ public class PropertyController {
     }
 
     @PostMapping("/property")
-    public ResponseEntity<Void> insert(Property property) throws NotValidException {
+    public ResponseEntity<Void> insert(@Valid @RequestBody Property property) throws NotValidException {
         propertyService.insert(property);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/property")
-    public ResponseEntity<Void> update(Property property) throws NotValidException {
+    public ResponseEntity<Void> update(@RequestBody Property property) throws NotValidException {
         propertyService.update(property);
         return ResponseEntity.ok().build();
     }
